0f4c91dcf91648a2eb26c7ffdcb4811a8ab4dc3e
Arslan Jan <Arslanjs.dev@gmail.com>
2023-03-08 23:00:10 +0500

    Customize sdk to suit Tour

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import FakeWindow from './components/FakeWindow'
+import CustomAgentUIExample from './CustomAgentUIExample'
+import './App.css'
+
+function App(props) {
+  function renderCustomerView () {
+    const params = {
+      cobrowseio_demo_id: props.demoId,
+      license: 'quGBhRTXlDPITg',
+      api: props.api,
+      device_name: 'Tour Website'
+    }
+    return null;
+    /*
+    return (
+      <div className='customer-view'>
+        <h2>Your Customer</h2>
+        <FakeWindow>
+          <iframe
+            className='device'
+            title='Device'
+            frameBorder={0}
+            width={'100%'}
+            height={'100%'}
+            src={`https://cobrowseio.github.io/todo-mvc-example/index.html?cobrowseio_demo_id=${params.cobrowseio_demo_id}&device_name=Web%20Trial%20Device&license=${params.license}&api=${params.api}`}
+          />
+        </FakeWindow>
+      </div>
+    )
+    */
+  }
+
+  function renderAgentView () {
+    return (
+      <div className='agent-view'>
+        <h2>Your Support Agent</h2>
+        <FakeWindow>
+            <CustomAgentUIExample {...props} />
+        </FakeWindow>
+      </div>
+    )
+  }
+
+  return (
+    <div className='App'>
+      {renderAgentView()}
+      {renderCustomerView()}
+    </div>
+  )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+const api = 'https://cobrowse.io'
+
+async function fetchToken (demoid) {
+  // !! WARNING READ THIS !!
+  // You should NOT use this endpoint to get your own token. This endpoint is
+  // specific to the cobrowse.io hosted online demo. It will not work with your
+  // account and devices.
+  // You should fetch a token in an authenticated way from your own server for
+  // the user. See our documentation on generating a JWT for cobrowse:
+  // https://docs.cobrowse.io/agent-side-integrations/custom-iframe-integrations/json-web-tokens-jwts
+  if (!demoid) return
+  window.localStorage.cobrowse_demo_id = demoid
+  // const res = await fetch(`${api}/api/1/demo/token?cobrowseio_demo_id=${demoid}`)
+  const res = await fetch(`http://localhost:8000/token`);
+  const { token } = await res.json()
+  return token
+}
+
+
+function render (token, demoId, api) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App token={token} demoId={demoId} api={api} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+}
+
+;(async function() {
+  const demoId = Date.now()*Math.random()
+  const token = await fetchToken(demoId)
+  render(token, demoId, api)
+})()
